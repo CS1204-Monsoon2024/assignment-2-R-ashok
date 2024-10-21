f96ccef666478b9650aa75b205301442474baf42
Riday C <144415546+R-ashok@users.noreply.github.com>
2024-10-21 23:56:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,94 @@
 #include "HashTable.cpp"
+#include <iostream>
+using namespace std;
 
-int main() {
-    int initialSize = 7; 
-    HashTable ht(initialSize);
+int nextprime(int k){
+    int i=2k+1;
+    bool found=false;
+    
+    while (!found){
+     bool isPrime=true;
+     for(int j=2; j*j<=i;j++){
+            if(i%j==0){
+             isPrime=false;
+             break;}
+        }
+
+        if(isPrime)
+         found=true;
+        else
+         i=i+2;
+    }
+ return i;
+ 
+ }
+//returns next prime number
+
+struct hash{
+    int* arr;
+    int size;
+    int currentsize;
+};
+
+int main[1000];
+
+hash make(int size){
+    int k=size;
+    for(int i=0; i<k; i++){
+        main[i]=-5;
+    }
+    struct hash n;
+    n.size=size;
+    n.currentsize=0;
+    n.arr=main;
+
+    return n;
+
+}
+
+void insertinto(struct hash k,int val){
+    int c= val%k.size;
+    for(int i=0; i<(k.size+1)/2; i++){
+     int quad=(c+i*i)%k.size;
+     if (k.arr[quad]==-5){
+        k.arr[quad]=val;
+        k.currentsize++;
+        return;
+     }
+     else if(k.arr[quad]==val){
+        cout << "Duplicate key insertion not allowed" << endl;
+        return;
+     }      
+    }
+  cout<< "no space in table" << endl;
+    return;}  
+
+void resize(hash k){
+    int oldsize=k.size;
+    k.size=nextprime(size);
+    int p=k.currentsize;
+    k.currentsize=0;
+    int* oldarr=k.arr;
+
+    for (int i=0;i<k.size;i++){
+        main_arr[i]=-5;
+    }
+
+    for (int i=0;i<oldsize; i++){
+        if (oldarr[i]!=-5){
+            k.currentsize++;
+            insertinto(k,oldarr[i]);
+        }
+}}
+
+
+
+int main(){
+    struct hash ht=make(7);
+
+    void insert(int val){
+        insertinto(ht,int val);
+    }
 
     // Example test case
     ht.insert(1);
